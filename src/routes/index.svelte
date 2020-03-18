d323b423bd6c3b7c4feebeac3ca8be576f1710da
maxfinnsjo <max.finnsjo@gmail.com>
2020-03-18 20:57:09 +0100

    Stealing thunder

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -33,14 +33,13 @@
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Odd Posts</title>
 </svelte:head>
 
 <h1>Great success rate > n!</h1>
 
 <figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH == FIVE!</figcaption>
+	<figcaption>HIGH == LIVE!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading. Did I do that?</strong></p>
+<p><strong>Did I do that?</strong></p>
